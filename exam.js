cfd53494c824f71cbf97536afb9d41d10f6404bf
ValentinAr7 <valio990210@gmail.com>
2022-06-11 21:05:48 +0100

    exam

---
+++ exam.js
@@ -0,0 +1,14 @@
+function exam(input){
+
+    let badGrades = Number(input[0]);
+    let index = 1;
+    let currentInput = input[index];
+
+    while (currentInput !== "Enough"){
+        
+    }
+
+
+}
+
+exam (["2", "Income", "3", "game info", "6", "Best Player", "4"])

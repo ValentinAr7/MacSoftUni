1359fda476bb8574aa398de15369f8380f175586
ValentinAr7 <valio990210@gmail.com>
2022-06-11 21:21:59 +0100

    update

---
+++ demo.js
@@ -0,0 +1,16 @@
+function demo(input){
+
+let needMoney = Number(input[0]);
+let currentMoney = Number(input[1]);
+let index = 2;
+let currentInput = input[index];
+let spendDays = 0;
+let days = 0;
+
+while(spendDays !== 5){
+    days++
+
+    
+}
+
+}
--- exam.js
+++ exam.js
@@ -3,12 +3,20 @@ function exam(input){
     let badGrades = Number(input[0]);
     let index = 1;
     let currentInput = input[index];
+    let sumGrades = 0;
 
-    while (currentInput !== "Enough"){
-        
+    while (badGrades < 2){
+        currentInput = input[index]
+        index++
+
+        if (currentInput === "Enough" )
+
+
+        break;
+
     }
 
 
 }
 
-exam (["2", "Income", "3", "game info", "6", "Best Player", "4"])
+exam (["2", "Income", "3", "game info", "6", "Best Player", "4", "Enough"])

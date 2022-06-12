3a03346bc7db97cea4a0691669a0ffd72c12d0a1
ValentinAr7 <valio990210@gmail.com>
2022-06-12 10:42:38 +0100

    else if

--- steps.js
+++ steps.js
@@ -6,12 +6,20 @@ let steps = Number(input[index]);
 let stepsCounter = 0;
 
 while(steps <= 10000){
-    stepsCounter = Number (input[index])
+    stepsCounter = input[index]
     index++
     steps += stepsCounter
 
     if(steps > 10000){
-        console.log(`Goal reached`);
+        console.log(`Goal reached! Good Job!`);
+        console.log(`${steps - stepsCounter} steps over the goal!`);
+
+    } else if (input[index] === "Going Home"){
+        stepsCounter = input[index]
+        index++
+        steps += stepsCounter
+        break;
+
     }
 
 
@@ -20,4 +28,4 @@ console.log(steps);
 
 }
 
-steps (["1000", "1500", "2000", " 6500"])
+steps (["1000", "1500", "Going Home", "2000", " 6500"])

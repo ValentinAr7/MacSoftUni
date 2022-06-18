9c312d8f7a6f1a6a85e39212e4c820f4bde88268
ValentinAr7 <valio990210@gmail.com>
2022-06-18 11:18:29 +0300

    start

---
+++ ftm.js
@@ -0,0 +1,13 @@
+function exercise(input){
+
+    let change = Number(input[o]);
+    let inCoins = Math.floor(change * 100);
+    let cpinsCounter = 0;
+
+    while(inCoins > 0){
+
+        
+    }
+
+
+}

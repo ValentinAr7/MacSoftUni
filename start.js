c629795a1ac8664f998cdf5728557fa8fd1e3480
ValentinAr7 <valio990210@gmail.com>
2022-06-19 23:27:41 +0300

    variables

--- start.js
+++ start.js
@@ -4,6 +4,17 @@ function start(input){
     let l = Number(input[1]);
     let h = Number(input[2]);
 
-    
+    let freeSpace = w * l * h
+
+    let index = 3;
+    let command = input[index];
+
+
 
 }
+start (["10",
+"1",
+"2",
+"4",
+"6",
+"Done"])

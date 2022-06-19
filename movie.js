6ae14305d2750ec1b73bcb2130ecf6b32fb13131
ValentinAr7 <valio990210@gmail.com>
2022-06-19 23:17:17 +0300

    cake

---
+++ movie.js
@@ -0,0 +1,12 @@
+function movie(input){
+
+let movieName = input[0]
+let sesons = Number(input[1]);
+let epizodes = Number(input[2]);
+let time = Number(input[3]);
+
+
+
+}
+
+movie(["Lucifer", "3" , "18", "55"])

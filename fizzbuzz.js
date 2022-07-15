5ce9a31899630b8db7213803e96d08dd6f54efb8
ValentinAr7 <valio990210@gmail.com>
2022-07-15 20:55:37 +0100

    else and lesi if

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,8 +5,12 @@ function fizzbuzz(){
     output.push(count);
     count++;
 
-    console.log(count);
-    
+    if(count % 3 == 0){
+        console.log("fizz");
+    } else if (count % 5 == 0){
+        console.log("fuzz");
+    } else if (count % 3 == 0 && count % 5 == 0) {
+        console.log(fizzfuzz);
 
 
 }

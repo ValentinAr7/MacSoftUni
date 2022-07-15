7638dc1ee2fd9eb28c021e8b57a45f751ffdf15c
ValentinAr7 <valio990210@gmail.com>
2022-07-15 21:34:58 +0100

    random generator

--- whoBuysFood.js
+++ whoBuysFood.js
@@ -1,8 +1,12 @@
 function food(names){
 
     let name = names.lenght
+    let randomPeople = Math.floor(Math.random * name)
+    let randomPerson = names[randomPeople];
+
+
     
-    console.log(name);
+
 
 
 }

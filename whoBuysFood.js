dbb32b5572b1f51bea8efd5f6b45909a4087da48
ValentinAr7 <valio990210@gmail.com>
2022-07-23 23:04:37 +0100

    change

--- whoBuysFood.js
+++ whoBuysFood.js
@@ -5,7 +5,7 @@ function food(names){
     let randomPerson = names[randomPeople];
 
 
-    console.log(`${randomPerson} is the random person`);
+    console.log(`${randomPerson} this is the random person`);
 
 
 
